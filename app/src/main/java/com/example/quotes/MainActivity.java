4a56110756ae3ff34dabc5033e0a20c99cc82475
Kiran Rana <kiranrana8973@gmail.com>
2019-03-11 17:18:43 +0545

    Added Adapter

--- app/src/main/java/com/example/quotes/MainActivity.java
+++ app/src/main/java/com/example/quotes/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.quotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView quotesList = findViewById(R.id.quoteList);
+        quotesList.setLayoutManager(new LinearLayoutManager(this));
+
+        List<String> lstQuote = new ArrayList<>();
+        lstQuote.add("Quote1");
+        lstQuote.add("Quote2");
+        lstQuote.add("Quote3");
+        lstQuote.add("Quote4");
+        lstQuote.add("Quote5");
+        lstQuote.add("Quote6");
+
+
+        QuotesAdapter adapter = new QuotesAdapter(this,lstQuote);
+        quotesList.setAdapter(adapter);
 
     }
 }
---
+++ app/src/main/java/com/example/quotes/QuotesAdapter.java
@@ -0,0 +1,58 @@
+package com.example.quotes;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.util.List;
+
+public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.QuotesViewHolder> {
+
+    Context context;
+    List<String> lstQuotes;
+
+    public QuotesAdapter(Context context, List<String> lstQuotes) {
+        this.context = context;
+        this.lstQuotes = lstQuotes;
+    }
+
+    @NonNull
+    @Override
+    public QuotesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.list_item_quotes,viewGroup,false);
+        return new QuotesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuotesViewHolder quotesViewHolder, int i) {
+        String[] colors ={"#FF4500","#FFD700","#FFA500","#FF8C00"};
+        String quotes = lstQuotes.get(i);
+        quotesViewHolder.txtQuotes.setText(quotes);
+        int color = i % colors.length;
+        int intColor = Color.parseColor(colors[color]);
+        quotesViewHolder.quoteContainer.setBackgroundColor(intColor);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstQuotes.size();
+    }
+
+    public class QuotesViewHolder extends RecyclerView.ViewHolder {
+        TextView txtQuotes;
+        LinearLayout quoteContainer;
+
+        public QuotesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtQuotes = itemView.findViewById(R.id.txtQuote);
+            quoteContainer = itemView.findViewById(R.id.quoteContainer);
+        }
+    }
+}
